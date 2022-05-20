9adc83ca65e5021ca7ca05d3b896ba6562d4ec8b
menso431 <menson.sundash@gmail.com>
2022-05-20 15:39:54 +1000

    Created Angular Components for Invoice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,27 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddInvoiceComponent } from './components/add-invoice/add-invoice.component';
+import { InvoiceDetailComponent } from './components/invoice-detail/invoice-detail.component';
+import { InvoiceListComponent } from './components/invoice-list/invoice-list.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    AddInvoiceComponent,
+    InvoiceDetailComponent,
+    InvoiceListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
